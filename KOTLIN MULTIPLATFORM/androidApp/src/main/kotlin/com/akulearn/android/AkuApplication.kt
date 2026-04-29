e817b90ef8c45bd8d8a9f04456dcaf627020e51c
UMAR ABUBAKAR <umarabubakarg2018@gmail.com>
2026-04-29 21:51:36 +0100

    Update AkuApplication.kt

--- KOTLIN MULTIPLATFORM/androidApp/src/main/kotlin/com/akulearn/android/AkuApplication.kt
+++ KOTLIN MULTIPLATFORM/androidApp/src/main/kotlin/com/akulearn/android/AkuApplication.kt
@@ -1,19 +1,18 @@
-package com.akulearn.android
+package com.akuplatform.android
 
 import android.app.Application
-import com.akulearn.android.di.androidModule
+import com.akuplatform.shared.di.initKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 
 class AkuApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-            androidLogger(Level.ERROR)
+
+        initKoin {
+            androidLogger()
             androidContext(this@AkuApplication)
-            modules(androidModule)
+            // If you use BuildConfig for keys, you can inject them here
         }
     }
 }
